f657b476ebfb977069c9ea94ed5f61024ff9a6f6
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-18 11:47:38 -0500

    Primer commit
    Iniciacion de proyecto

---
+++ LoginNetCoreMVC/LoginNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
